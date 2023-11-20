80804e7e7dd8ed0aa053b87282e84178dccc0fe0
Minyoung Lee <mminy62@gmail.com>
2023-11-20 18:36:29 +0900

    Create ParkingApp
    공영주차장 이용 앱

---
+++ Project/ParkingApp/ParkingApp/ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingAppApp.swift
+//  ParkingApp
+//
+//  Created by 이민영 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
