e39737816b1a974c1bd2c60662c912995e123974
funMango <138420832+funMango@users.noreply.github.com>
2023-11-24 13:04:42 +0900

    ParkingApp first commit

---
+++ Project/ParkingApp/ParkingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkingApp
+//
+//  Created by 이민호 on 11/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
